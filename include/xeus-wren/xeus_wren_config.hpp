be0cf8ac303c4f2ea7557451fad916e54d587955
DerThorsten <derthorstenbeier@gmail.com>
2021-10-12 14:47:31 +0200

    bumped version

--- include/xeus-wren/xeus_wren_config.hpp
+++ include/xeus-wren/xeus_wren_config.hpp
@@ -10,8 +10,8 @@
 
 // Project version
 #define XEUS_WREN_VERSION_MAJOR 0
-#define XEUS_WREN_VERSION_MINOR 1
-#define XEUS_WREN_VERSION_PATCH 2
+#define XEUS_WREN_VERSION_MINOR 2
+#define XEUS_WREN_VERSION_PATCH 0
 
 // Composing the version string from major, minor and patch
 #define XEUS_WREN_CONCATENATE(A, B) XEUS_WREN_CONCATENATE_IMPL(A, B)
